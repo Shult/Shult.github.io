57d00743603168898eb8ab17ab2bdb1e51481586
Shult <mestres.sin@gmail.com>
2022-07-28 08:54:45 +0000

    test typescript Button

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ function sayHelloWorld() {
     console.log("\n");
     console.log("*** Hello World from Type Script code. Update test ***");
     console.log("\n");
+    var btn = document.getElementById("createChannel");
+    btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", handleClick);
+    function handleClick() {
+        console.log("Clicked!");
+        this.removeEventListener("click", handleClick);
+    }
 }
 sayHelloWorld();
--- script.ts
+++ script.ts
@@ -2,6 +2,14 @@ function sayHelloWorld(): void {
   console.log("\n");
   console.log("*** Hello World from Type Script code. Update test ***");
   console.log("\n");
+  let btn = document.getElementById("createChannel");
+  btn?.addEventListener("click", handleClick);
+
+  function handleClick() {
+    console.log("Clicked!");
+    this.removeEventListener("click", handleClick);
+  }
+
 }
 
 sayHelloWorld();
