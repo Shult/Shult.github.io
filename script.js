bae3f72f1088ccb8a71cb77f07878699ead31911
Shult <mestres.sin@gmail.com>
2022-07-28 09:11:15 +0000

    test typescript createChannel 1

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+"use strict";
+exports.__esModule = true;
+var I_ownerCreateChannel_1 = require("../integration-services/clients/client-sdk/examples/I-ownerCreateChannel");
 function sayHelloWorld() {
     console.log("\n");
     console.log("*** Hello World from Type Script code. Update test ***");
     console.log("\n");
     var btn = document.getElementById("createChannel");
     btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", handleClick);
+    (0, I_ownerCreateChannel_1.ownerCreateChannel)();
     function handleClick() {
         console.log("Clicked!");
         this.removeEventListener("click", handleClick);
--- script.ts
+++ script.ts
@@ -1,10 +1,12 @@
+import { ownerCreateChannel } from '../integration-services/clients/client-sdk/examples/I-ownerCreateChannel';
+
 function sayHelloWorld(): void {
   console.log("\n");
   console.log("*** Hello World from Type Script code. Update test ***");
   console.log("\n");
   let btn = document.getElementById("createChannel");
   btn?.addEventListener("click", handleClick);
-
+  ownerCreateChannel();
   function handleClick() {
     console.log("Clicked!");
     this.removeEventListener("click", handleClick);
